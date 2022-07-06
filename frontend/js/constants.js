51f73766ed0235c61aa586367f9fc026a4b22baf
Gustavo <gustavocmalbertini@outlook.com>
2022-07-06 17:27:35 -0400

    update text test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -4,6 +4,7 @@ const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 const welcome_h1 = "Welcome to the Masuku Girls NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
 const welcome_p = 'The MasukuGirls NFT Project that aims how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p2 = 'launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.'
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
